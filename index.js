0d34d70c1992963c2029ef99d6bbaa1c42ce3994
MiniOcean404 <1037306928@qq.com>
2023-08-27 00:14:51 +0800

    🐛 test

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-/*
-测试脚本
-@Leaf
+/**
+  秒杀美团优惠券 v1.01
 
-cron: 33 7,22 * * *
-*/
+  cron: 52 0,6-22/2 * * *
+  const $ = new Env("秒杀美团优惠券");
+ */
 
 const { default: axios } = require('axios')
 
 const init = async () => {
   console.log(process.env)
 
-  // const res = await axios.get('http://www.baidu.com')
-  // console.log(res.status)
+  const res = await axios.get('http://www.baidu.com')
+  console.log(res.status)
 }
 
 init()
